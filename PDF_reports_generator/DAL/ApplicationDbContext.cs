40838f80c5d1ffc7ef029e37d8a7cfb8938f9bb2
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-11-15 19:56:20 +0300

    Delete unnecessary files & Reconsider file structure & Add initial entities for EF

---
+++ PDF_reports_generator/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
