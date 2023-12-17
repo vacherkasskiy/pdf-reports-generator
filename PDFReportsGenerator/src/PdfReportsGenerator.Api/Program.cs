fb27bd764d32e8e65bd2699b5e14d91ebedc32e6
vacherkasskiy <vitalik.cherkasskiy@gmail.com>
2023-12-18 00:42:14 +0300

    Add sentry

--- PDFReportsGenerator/src/PdfReportsGenerator.Api/Program.cs
+++ PDFReportsGenerator/src/PdfReportsGenerator.Api/Program.cs
@@ -7,17 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Host.UseSerilog();
+
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = "https://5a737d8c912e898e433e1b55ecebe9a6@o4506411489361920.ingest.sentry.io/4506413229604864";
+    o.Debug = true;
+    o.TracesSampleRate = 1.0;
+});
 
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
-
-builder.Host.UseSerilog();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -35,5 +41,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSentryTracing();
 
 app.Run();
